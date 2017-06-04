0fb77c231524338c9a785bc38a9f97aa4227c2c0
cthedot <cthedot@gmail.com>
2017-06-04 14:52:35 +0200

    Split css in base and text. added fontobserver, added negative

---
+++ js/enablefonts.js
@@ -0,0 +1,41 @@
+;
+(function () {
+  "use strict";
+
+  /*
+    call at end of HTML (before onload or onready):
+
+    enableFonts([{
+      name: 'Source Sans Pro'
+    }, {
+      name: 'Source Sans Pro',
+      variation: {
+        weight: 600
+        // style: 'italic'
+      }
+    }])
+  */
+
+  window.enableFonts = function(fontsConfig) {
+    var html = document.documentElement;
+    var script = document.createElement("script");
+    script.src = "js/lib/fontfaceobserver.js";
+    script.async = true;
+    script.onload = function () {
+      var fonts = []
+
+      fontsConfig.forEach(function(font, i) {
+        fonts.push(new FontFaceObserver(font.name, font.variation || undefined))
+      })
+      Promise.all(
+        fonts.map(function (font) {
+          return font.load()
+        })
+      ).then(function () {
+        html.classList.add("fonts-loaded");
+      });
+    };
+    document.head.appendChild(script);
+  }
+
+})();
---
+++ js/script.js
@@ -0,0 +1,35 @@
+;
+(function () {
+  "use strict";
+
+  // WEBFONTS see CSS
+  enableFonts([{
+    name: 'Open sans',
+    variation: {
+      weight: 300
+    }
+  }, {
+    name: 'Open sans',
+    variation: {
+      weight: 300,
+      style: 'italic'
+    }
+  }, {
+    name: 'Open sans',
+    variation: {
+      weight: 400
+    }
+  }, {
+    name: 'Open sans',
+    variation: {
+      weight: 400,
+      style: 'italic'
+    }
+  }, {
+    name: 'Open sans',
+    variation: {
+      weight: 600
+    }
+  }]);
+
+})();
